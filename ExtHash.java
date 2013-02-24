0288026f328131fdb437caf3b721764e2bc41905
Ryan Gell <rtgell@gmail.com>
2013-02-24 11:43:41 -0500

    Update ExtHash

--- ExtHash.java
+++ ExtHash.java
@@ -39,6 +39,7 @@ public class ExtHash <K, V>
         int  nKeys;
         K [] key;
         V [] value;
+        //2^d = depth for some d
         int depth;
         @SuppressWarnings("unchecked")
         Bucket (int d)
@@ -52,6 +53,11 @@ public class ExtHash <K, V>
         	return depth;
         }
     } // Bucket inner class
+    /***
+    *Defines a Map Entry to return for the entrySet function implements the Map.Entry
+    *Interface
+    *@author Ryan Gell
+    */
     private class Entry implements Map.Entry <K, V>
     {
     	    K key;
@@ -111,6 +117,7 @@ public class ExtHash <K, V>
         hTable = new ArrayList <Bucket> ();   // for bucket storage
         dir    = new ArrayList <Bucket> ();   // for bucket access
         mod    = nBuckets = initSize;
+        //Initialize empty buckets into directory and hTable
         for(int i = 0; i < initSize; i++){
         	Bucket b = new Bucket(mod);
         	hTable.add(b);
@@ -121,18 +128,19 @@ public class ExtHash <K, V>
     /***************************************************************************
      * Return a set containing all the entries as pairs of keys and values.
      * @return  the set view of the map
+     * @author Ryan Gell
      */
     public Set <Map.Entry <K, V>> entrySet ()
     {
-        Set <Map.Entry <K, V>> enSet = new HashSet <Map.Entry <K, V>> ();
-        for(int i = 0; i < hTable.size(); i++){
-        	Bucket buck = hTable.get(i);
-        	for(int j = 0; j < buck.nKeys; j++){
-        		enSet.add(new Entry(buck.key[j], buck.value[j]));
+        Set <Map.Entry <K, V>> enSet = new HashSet <Map.Entry <K, V>> ();//Intilize set
+        for(int i = 0; i < hTable.size(); i++){//For every bucket
+        	Bucket buck = hTable.get(i);//Get the bucket
+        	count++;
+        	for(int j = 0; j < buck.nKeys; j++){//For every key
+        		enSet.add(new Entry(buck.key[j], buck.value[j]));//Create the key-value pair entry and add it to the set
         	}
         }    
-    
-        return enSet;
+        return enSet;//Return the set
     } // entrySet
 
     /***************************************************************************
@@ -144,10 +152,15 @@ public class ExtHash <K, V>
     {
         int    i = h (key);
         Bucket b = dir.get (i);
+        count++;
+        //Return null if key is not in bucket
         V ret = null;
         for(int j = 0; j < b.nKeys; j++){
+        	//Search bucket for key
         	if(b.key[j].equals(key)){
+        		//If you find the key, set ret;
         		ret = b.value[j];
+        		j = b.nKeys; //break
         	}
         }
 
@@ -159,83 +172,115 @@ public class ExtHash <K, V>
      * @param key    the key to insert
      * @param value  the value to insert
      * @return  null (not the previous value)
+     * @author Ryan Gell
      */
     public V put (K key, V value)
     {
         int    i = h (key);
         Bucket b = dir.get (i);
-
+        count++;
+        //If the key is already in the bucket
+        //We just want to replace it with the new value
         for(int j = 0; j < b.nKeys; j++){
         	if(b.key[j].equals(key)){
         		b.value[j] = value;
         		return null;
         	}
         }
+        //Else if there is room, just put the key at the end of the bucket
         if(b.nKeys < SLOTS){
         	b.key[b.nKeys] = key;
         	b.value[b.nKeys] = value;
         	b.nKeys++;
         	return null;
         }
+        //Else there is no room in the bucket, so you need to split the bucket to make room
         else{
-        	expand(b, key, value);
+        	split(b, key, value);
         	return null;
         }
     } // put
-    private void expand(Bucket b, K key, V value){
-    	    int depth;
+    /**
+    * Takes in a bucket. Splits the bucket to make room for a new key-value pair.
+    * If the bucket is already at maximum depth, the hashfunction will be increased.
+    * 
+    * @author Ryan Gell
+    */
+    private void split(Bucket b, K key, V value){
+    	    int depth; 
+    	    //If the bucket is at maximum depth
     	    if(b.getDepth() == mod){
     	    	    int size = dir.size();
+    	    	    //Double the directory
     	    	    for(int i = 0; i < size; i++){
     	    	    	    dir.add(dir.get(i));
     	    	    }
+    	    	    //Find where the first bucket will be
     	    	    int i = h(key);
+    	    	    //Where the second bucket will be
     	    	    int j = i+mod;
+    	    	    //double the mod
     	    	    mod = mod*2;
-    	    	    depth = mod;
+    	    	    //Create new buckets
     	    	    Bucket buck1 = new Bucket(mod);
     	    	    Bucket buck2 = new Bucket(mod);
     	    	    nBuckets++;
+    	    	    //Delete the old bucket from hTable
     	    	    for(int k = 0; k < hTable.size(); k++){
+    	    	    	    count++;
     	    	    	    if(hTable.get(k).equals(b)){hTable.remove(k); k = hTable.size();}
     	    	    }
+    	    	    //Remove the old buckets
     	    	    dir.remove(i);
+    	    	    //Add the new one
     	    	    dir.add(i, buck1);
     	    	    dir.remove(j);
     	    	    dir.add(j, buck2);
-    	    	    hTable.add(buck1);
+    	    	    hTable.add(buck1);//Into storage
     	    	    hTable.add(buck2);
     	    }
     	    else{
+    	    	    //Get current depth
     	    	    int d = b.getDepth();
     	    	    int i = 0;
+    	    	    //Find the first instance of this bucket
     	    	    for(int l = 0; l < dir.size(); l++){
+    	    	    	    count++;
     	    	    	    if(b.equals(dir.get(l))){
     	    	    	       i = l;
     	    	    	       l = dir.size();
     	    	    	    }
     	    	    }
+    	    	    //Create buckets at new depth
     	    	    Bucket buck1 = new Bucket(d*2);
     	    	    Bucket buck2 = new Bucket(d*2);
     	    	    int k = 0;
+    	    	    //Delete old bucket
     	    	    for(int l = 0; l < hTable.size(); l++){
+    	    	    	    count++;
     	    	    	    if(hTable.get(l).equals(b)){hTable.remove(l); l = hTable.size();}
     	    	    }
+    	    	    //Add new ones
     	    	    hTable.add(buck1);
     	    	    hTable.add(buck2);
     	    	    while(i < mod){
+    	    	    	    //Place the first bucket every other time
     	    	    	    if(k % 2 == 0){
     	    	    	    	    dir.remove(i);
     	    	    	    	    dir.add(i, buck1);
     	    	    	    }
+    	    	    	    //Place the second bucket
     	    	    	    else{
     	    	    	    	    dir.remove(i);
     	    	    	    	    dir.add(i, buck2);
     	    	    	    }
     	    	    	    k++;
+    	    	    	    //Increment the position by the original depth
     	    	    	    i = i + d;  	    
     	    	    }
     	    }
+    	    //Place all the key-value pairs in the expanded hashtable.
+    	    //Note this may cause another split
     	    put(key, value);
     	    for(int k = 0; k < b.nKeys; k++){
     	    	    put(b.key[k], b.value[k]);
@@ -252,12 +297,14 @@ public class ExtHash <K, V>
 
     /***************************************************************************
      * Print the hash table.
+     * @author Ryan Gell
      */
     public void print(){
         out.println ("Hash Table (Extendable Hashing)");
         out.println ("-------------------------------------------");
         for(int i = 0; i < dir.size(); i++){
         	Bucket b = dir.get(i);
+        	count++;
         	System.out.println("Bucket " + i + ":" + " Depth:" + b.getDepth());
         	for(int j = 0; j < b.nKeys; j++){
         		System.out.println("Key:" + b.key[j] + " value:"+b.value[j] );
